e9ff8ad0cf0c263b82ce2479dfd7be24f04ccb32
Hailey-Z <hailey.zhang1@monash.edu>
2023-01-27 12:25:58 +1100

    updated to v0.12

--- global.R
+++ global.R
@@ -33,4 +33,4 @@ library("shinycssloaders") # new added
 library("shiny.info") # new added
 library("ggVennDiagram") # new added
 library("scales") # new added
-VERSION <- "v0.11"
+VERSION <- "v0.12"
