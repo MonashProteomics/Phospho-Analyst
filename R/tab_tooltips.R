fe8b93c4a4c3324521df6c3a122379b54a5012d6
Hailey-Z <hailey.zhang1@monash.edu>
2023-08-08 16:45:08 +1000

    updated tooltips

--- R/tab_tooltips.R
+++ R/tab_tooltips.R
@@ -3,8 +3,9 @@
 tooltips_ui <- function(tab){
   title = switch (tab,
                   # common plots
-                  "Volcano plot" = "For each pairwise comparison, volcano plot illustrates statistically significant (p-value) versus change magnitude (fold change)",
-                  "Heatmap" = "The heatmap provides an overview of all differentially expressed proteins (rows) across all samples (columns) and clustered hierarchically based on similar expressions",
+                  "Volcano plot" = 'A volcano plot maps the “log2 fold changes” on the x-axis against the –log10 “p-values” on the y-axis.
+                                    Potentially interesting candidate proteins are located in the left and right upper quadrant',
+                  "Heatmap" = "The heatmap provides an overview of all differentially expressed phosphosites or proteins (rows) across all samples (columns) and clustered hierarchically based on similar expressions",
                   "Individual Plot" = "Individual intensities of a given phosphosite or protein are plotted across all replicates of a condition either as box plot, 
                                        violin plot, interaction plot or intensity plot",
                   "PCA Plot" = "A Principal Component Analysis (PCA) is a technique used to emphasize variation and bring out strong patterns in a dataset. 
@@ -19,33 +20,31 @@ tooltips_ui <- function(tab){
                   "Missing values - Heatmap" = "Indicates whether a value of a given protein (rows) in a given sample (columns) is missing (0; white) or not (1; black). 
                                                 Only phosphosites or proteins with at least one missing value are visualized",
                   "Imputation" = "A density plot of phosphosite or protein intensity (log2) distribution for each condition after and before missing value imputation being performed",
-                  "Gene Ontology/ Pathway" = "Enrichment analyses performed by using the protein IDs of significantly regulated phosphosites, 
-                                              three Gene Ontology terms and two pathway databases can be selected",
-                  "Kinase-Substrate enrichment" = "A summary bar plot of kinase active scores based on significantly regulated phosphosites",
+                  "Gene Ontology/ Pathway" = "The protein IDs of significantly regulated phosphosites are used for the enrichment analysis. 
+                                              Three Gene Ontology terms in addition to KEGG and Reactome  can be selected",
+                  "Kinase-Substrate enrichment" = "A tool to estimate changes in kinase activities using bar plots",
                   
                   # special on Phosphosite page
-                  "Abundance rank" = "A rank/abundance plot illustrates a dynamic distribution of phosphosites by plotting the mean abundance values against 
-                                      corresponding rank",
-                  "Abundance comparison" = "A scatter plot to represent the comparison of mean phosphosite abundance between each pairwise across all phosphosites 
-                                            with a 1:1 dashed line as reference",
+                  "Abundance rank" = "A plot illustrating the dynamic distribution of all phosphosites",
+                  "Abundance comparison" = "A scatter plot comparing mean phosphosite abundances across experimental conditions,  with a 1:1 dashed line as reference",
                 
                   # special on ProteinGroup page
                   "Protein Numbers" = "The number of identified and quantified proteins in each sample",
-                  "Gene Ontology" = "Perform enrichment analyses of significantly regulated proteins by a selection of three Gene Ontology terms",
-                  "Pathway enrichment" = "Perform enrichment analyses of significantly regulated proteins by a selection of two pathway enrichment databases",
+                  "Gene Ontology" = "A tool to perform enrichment analyses of significantly regulated proteins using Gene Ontology terms",
+                  "Pathway enrichment" = "A tool to erform enrichment analyses of significantly regulated proteins using the KEGG or Reactome database",
                   
                   # special on Comparison page
                   "Numbers" = "The number of identified and quantified phosphosites or proteins in each sample",
-                  "Interaction plot" = "Combined data to show the interaction between different replicates. Enabling distinction of abundance at the protein and phosphosite level",
-                  "Bubble plot" = "Displays the phosphosite intensity values of a selected protein group. Phosphosites are coloured by experiment conditions, 
-                                    and the size of points represents the mean intensity values of a same phosphosite",
+                  "Interaction plot" = "Side-by-side comparison of protein and associated phosphosite intensities plotted across all individual replicates of all conditions",
+                  "Bubble plot" = "Displays phosphosite intensity values of selected proteins. Phosphosites are coloured by experimental conditions, 
+                                  and the size of each bubble correlates with the phosphosite’s mean intensity",
                   # special on Phosphosite(corrected) page
                   "Normalization (normal vs corrected)" = "These two plots represent the normal and corrected phosphosite intensity distribution in each sample",
-                  "Imputation (normal vs corrected)" = "A density plot of phosphosite intensity (log2) distribution for each condition after and before protein abundances correction being performed",
+                  "Imputation (normal vs corrected)" = "Density plots of phosphosite intensity (log2) distribution for each condition before and after correction for protein abundances",
                   
                   # Absence/Presence page
-                  "Venn Plot" = "Illustrates relationships between two or three selected conditions, colored by the counted number of phosphosites or proteins",
-                  "UpSet Plot" = "Visualize intersections between intensity columns. For each sample(row), the cells that are part of an intersection are filled in"
+                  "Venn Plot" = "Qualitative overlap of identified phosphosites between two or three selected conditions shown as overlapping circles",
+                  "UpSet Plot" = "Qualitative overlap of identified phosphosites between any number of condition(s)"
                   
   )
   span(tab, title = title,`data-toggle`="tooltip")
